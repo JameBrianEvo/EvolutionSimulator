7eda82558eaf86364554d7775d90ce1121eb07bc
brainsmarts <59645655+brainsmarts@users.noreply.github.com>
2024-07-08 17:01:10 -0400

    bugfix: change grid layers
    context:
    fixed the mac scripts not updating, properly tested and realized that the grid was not on the proper layer
    
    update:
    obstacle layer and border layer are now on layer 6, making it visible to the ray cast
    
    testing:
    Ran the simulation
    It changed directions when

--- My project/Assets/Scripts/CreatureLogic/ActionUtils.cs
+++ My project/Assets/Scripts/CreatureLogic/ActionUtils.cs
@@ -7,16 +7,16 @@ public static class ActionUtils {
 
     public static bool IsObstacleDetected(Rigidbody2D rb)
     {
-        Debug.Log("Detecting Obstacle");
-        int layerMask = 1 << obstacleLayer;
 
-        RaycastHit2D hit = Physics2D.Raycast(rb.position, rb.velocity, UnitUtilities.TILE, layerMask);
+        int layerMask = 1 << obstacleLayer;
         Vector2 rayDirection = rb.velocity.normalized * UnitUtilities.TILE;
 
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, rb.velocity, UnitUtilities.TILE, layerMask);
         //For Us to see where the ray is being cast
         Debug.DrawLine(rb.position, rb.position + rayDirection, Color.red);
-        if(hit.collider != null) { 
-        
+
+        if(hit.collider != null) {
+
             return true;
         }
 
