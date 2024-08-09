128105d6fd1824945dd0d7e6a6ca4fff6bd24360
brainsmarts <59645655+brainsmarts@users.noreply.github.com>
2024-08-09 18:49:39 -0400

    feat: added logging
    ### context
    We needed some way to keep track of sessions
    
    ### update
    - added a method to write to a file
    - added a method to calculate average meta data
    - added a method to calculate most common trait of some subtrait such as Diet or Sleep pattern
    
    testing
    - debug.log statements
    - checking the log to see if its valid

--- My project/Assets/Scripts/CreatureLogic/CreatureManager.cs
+++ My project/Assets/Scripts/CreatureLogic/CreatureManager.cs
@@ -24,8 +24,10 @@ public class CreatureManager : MonoBehaviour
     void Start()
     {
         instance = this;
-        
-        
+        string path = Application.dataPath + FileName;
+        StreamWriter writer = new(path, false);
+        writer.Write("");
+        writer.Close();
     }
 
     void FixedUpdate(){
@@ -38,18 +40,68 @@ public class CreatureManager : MonoBehaviour
         }
     }
 
-    public void LogCreatureStates(){
-        string path = Application.persistentDataPath + FileName;
-        StreamWriter writer = new(path, true);
+    private void LogCreatureStates(){
+        string path = Application.dataPath + FileName;
+        StreamReader reader = new(path, false);
+        string content = reader.ReadToEnd();
+        reader.Close();
+
+        StreamWriter writer = new(path, false);
         List<BaseCreature> listOfCreatures = new List<BaseCreature>(gameObject.GetComponentsInChildren<BaseCreature>());
-        float AvSpeed = 0, AvRange = 0;
-        foreach(BaseCreature creature in listOfCreatures){
-            AvRange += creature.data.attributesData.sightRange;
-            AvSpeed += creature.data.movementData.speed;
-        }
-        writer.WriteLine("Log");
-        writer.WriteLine("Average Speed" + AvSpeed / listOfCreatures.Count);
-        writer.WriteLine("Average Range" + AvRange / listOfCreatures.Count);
+
+        writer.Write(content);
+        writer.Write("Number of creatures: " + listOfCreatures.Count);
+        writer.Write(GetAverageMetaData(listOfCreatures));
+        writer.Write(GetAverageTrait(listOfCreatures, Traits.FOOD, FoodTraits.DIET));
         writer.Close();
+        
+    }
+
+    private string GetAverageMetaData(List<BaseCreature> creatures)
+    {
+        string averages = "";
+        float avSpeed = 0, avRange = 0, avCurEngergy = 0;
+        foreach (BaseCreature creature in creatures)
+        {
+            avRange += creature.data.attributesData.sightRange;
+            avSpeed += creature.data.movementData.speed;
+            avCurEngergy += creature.data.energyData.currentEnergy;
+        }
+        avSpeed /= creatures.Count;
+        avRange /= creatures.Count;
+        avCurEngergy /= creatures.Count;
+        averages += "\nAverage Speed of a creature: " + avSpeed;
+        averages += "\nAverage Range of a creature: " + avRange;
+        averages += "\nAverage Energy of a creature: " + avCurEngergy;
+        averages += "\n";
+        return averages;
+    }
+
+    private string GetAverageTrait(List<BaseCreature> creatures, Traits category, int trait)
+    {
+        string averages;
+        int common;
+        int count = 0;
+        common = creatures[0].data.traitData.traits[category][trait] & (int)Traits.SUBMASK;
+        for (int i = 0; i < creatures.Count; i++)
+        {
+            //Debug.Log("Checking Trait: " +(creatures[i].data.traitData.traits[category][trait] & (int)Traits.SUBMASK));
+            if((common & (int)Traits.SUBMASK) == (creatures[i].data.traitData.traits[category][trait] & (int)Traits.SUBMASK))
+            {
+                count += 1 ;
+            }
+            else
+            {
+                count -= 1;
+                if(count < 0)
+                {
+                    count = 0;
+                    common = creatures[i].data.traitData.traits[category][trait] & (int)Traits.SUBMASK;
+                }
+            }
+        }
+        averages = "Most common " + category + " " + trait + " is " + common;
+        averages += "\n";
+        return averages;
     }
 }
